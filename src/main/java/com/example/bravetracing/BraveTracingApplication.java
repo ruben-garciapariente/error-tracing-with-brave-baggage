d28fd70554eec2a6de1cc82a15d094dab25e1f7a
n129586 <ruben.garciapariente@gruposantander.es>
2023-02-06 13:16:35 +0100

    Error with baggage configure by application.yaml

--- src/main/java/com/example/bravetracing/BraveTracingApplication.java
+++ src/main/java/com/example/bravetracing/BraveTracingApplication.java
@@ -1,16 +1,8 @@
 package com.example.bravetracing;
 
-import brave.baggage.BaggageField;
-import brave.baggage.BaggagePropagation;
-import brave.baggage.BaggagePropagationConfig;
-import brave.baggage.BaggagePropagationCustomizer;
-import brave.baggage.CorrelationScopeConfig;
-import brave.baggage.CorrelationScopeCustomizer;
-import brave.baggage.CorrelationScopeDecorator;
 import io.micrometer.context.ContextSnapshot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
@@ -18,21 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BraveTracingApplication {
-
-	private static final BaggageField TEST1 = BaggageField.create("TEST1");
-
-	@Bean
-	CorrelationScopeCustomizer baseBaggageFieldsCorrelationScope() {
-		return (CorrelationScopeDecorator.Builder builder) -> builder
-				.add(CorrelationScopeConfig.SingleCorrelationField.newBuilder(TEST1).flushOnUpdate()
-						.build());
-	}
-
-	@Bean
-	BaggagePropagationCustomizer baseBaggagePropagation() {
-		return (BaggagePropagation.FactoryBuilder factoryBuilder) -> factoryBuilder
-				.add(BaggagePropagationConfig.SingleBaggageField.remote(TEST1));
-	}
 
 	// Example of Async Servlets setup
 	@Configuration(proxyBeanMethods = false)
